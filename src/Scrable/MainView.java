e0a46d0254d04d291917131be4ae99e156683d8e
Yun Zheng <yunzheng2@unm.edu>
2022-09-28 22:52:16 -0600

    try to make GUI

--- src/Scrable/MainView.java
+++ src/Scrable/MainView.java
@@ -2,7 +2,13 @@ package Scrable;
 
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.nio.Buffer;
 
 public class MainView extends Application {
 
@@ -10,8 +16,18 @@ public class MainView extends Application {
         launch(args);
     }
 
+    Button btn;
     @Override
     public void start(Stage primaryStage) {
         // set up GUI here
+        btn = new Button();
+        btn.setText("Click Me");
+        BorderPane pane= new BorderPane();
+        pane.setCenter(btn);
+        Scene scene= new Scene(pane,300,200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
     }
 }
