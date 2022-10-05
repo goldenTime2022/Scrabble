27e4dfcede8146ee8764c59d78114b25ff20cd0c
Yun Zheng <yunzheng2@unm.edu>
2022-10-05 13:41:58 -0600

    scanner the dictionary

--- src/Scrable/MainView.java
+++ src/Scrable/MainView.java
@@ -9,60 +9,34 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.Buffer;
+import java.util.Scanner;
 
 public class MainView extends Application {
-    private static String[] clargs;
+    //private static String[] clargs;
 
     public static void main(String[] args) {
-        clargs = args;
+        //clargs = args;
         launch(args);
-        // BufferedReader br = null;
-        //System.out.println("Which dictionary files you want to load?");
-        /*try{
-            //use "sowpods.txt"
-            br= new BufferedReader(new FileReader(args[1]));
-            String line;
-        }catch(IOException e){
-            e.printStackTrace();
-        }*/
     }
 
     @Override
     public void start(Stage primaryStage) {
         // set up GUI here
-        BufferedReader br = null;
-        //System.out.println("Which dictionary files you want to load?");
-        try {
-            //use "sowpods.txt"
-            if (clargs.length == 0) {
-                System.out.println("missing commandline.");
-                display("Missing commandline argument - dictionary file name.");
-                return;
+        System.out.println("What is the file Name: ");
+        File file = new File("C:\\Users\\yun\\IdeaProjects\\scrabblegame\\dictionaryFile\\sowpods.txt");
+        try{
+            Scanner scnr = new Scanner(file);
+            while(scnr.hasNextLine()){
+                String s= scnr.nextLine();
+                System.out.println(s);
             }
-
-            br = new BufferedReader(new FileReader("dictionaryFile/"+clargs[0]));
-            // write code to read
-            String line;
-            //while loop here to read line
-            while((line= br.readLine())!=null){
-                line=line.trim();
-                System.out.println(line);
-            }
-            br.close();
-        } catch (IOException e) {
-            display("Unable to open file: "+ clargs[0]);
-            //e.printStackTrace();
+            scnr.close();
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-    }
-    private void display(String msg){
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Error");
-        alert.setHeaderText(null);
-        alert.setContentText(msg);
-        alert.show();
+
     }
 }
+
