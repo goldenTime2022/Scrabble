8e135e2e4847db6167e78fbf322d04f2eec65a6a
Yun Zheng <yunzheng2@unm.edu>
2022-10-16 11:27:05 -0600

    just fix the scan board into 2d array problem!!!!!

--- src/Scrable/MainView.java
+++ src/Scrable/MainView.java
@@ -76,145 +76,92 @@ public class MainView extends Application {
         //System.out.println("----------------------------------------");
 
 
-////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////
         //start scan the board
         File fileBoard = new File("dictionaryFile/small_board.txt");
         try {
             Scanner scan_Board = new Scanner(fileBoard);
             String d = scan_Board.nextLine();
             int dimension = Integer.parseInt(d);
-            scan_Board.useDelimiter(" ");
-            String value;
+            String[] value;
             char[] charArr;
             Square[][] square = new Square[dimension][dimension];
-            for (int i = 0; scan_Board.hasNextLine(); i++) {
-                for (int j = 0; j<dimension; j++) {
-                    if (scan_Board.hasNext()) {
-                        value = scan_Board.next();// this is the one cause trouble???????????????????????? this scan the extra one
-                        System.out.print("j = " + j + "; value = " + value + " // ");
-                        charArr = value.toCharArray();
-                        square[i][j] = new Square(0, 0, false, null);
-                        //System.out.println("charArr[0] = " + charArr[0]);
-                        //System.out.println("charArr[1] = " + charArr[1]);
-                        if (charArr[0] == ' ') {
-                            square[i][j].setOccupied(true);
-                        } else if (charArr[0] == '.' && charArr[1] == '.') {
-                            square[i][j].setWordMultiplier(0);
-                            square[i][j].setLetterMultiplier(0);
-                        } else if (charArr[0] == '.') {
-                            square[i][j].setLetterMultiplier(charArr[1] - 48);
-                        } else if (charArr[1] == '.') {
-                            square[i][j].setWordMultiplier(charArr[0] - 48);
-                        }
-                        //System.out.println("square[" + i + "][" + j + "] = " + square[i][j]);
+            for (int row = 0; row < dimension; row++) {
+                String line = scan_Board.nextLine();
+                value = line.split(" ");
+                for (int col = 0; col < dimension; col++) {
+                    charArr = value[col].toCharArray();
+                    //System.out.println("@ row = " + row + "; column = " + col +"; value = " + value[col] + "; charArr[0] = " + charArr[0] + "; charArr[1] = " + charArr[1] );
+                    square[row][col] = new Square(0, 0, false, null);
+                    if (charArr[0] == ' ') {
+                        square[row][col].setOccupied(true);
+                    } else if (charArr[0] == '.' && charArr[1] == '.') {
+                        square[row][col].setWordMultiplier(0);
+                        square[row][col].setLetterMultiplier(0);
+                    } else if (charArr[0] == '.') {
+                        square[row][col].setLetterMultiplier(charArr[1] - 48);
+                    } else if (charArr[1] == '.') {
+                        square[row][col].setWordMultiplier(charArr[0] - 48);
                     }
-                    //System.out.print(square[i][j] +" ");
+                    System.out.print(square[row][col] + " ");
                 }
                 System.out.println();
-                System.out.println("--------------------------------------------");
             }
             scan_Board.close();
-        }catch(FileNotFoundException e){
-            e.printStackTrace();
-        }
 
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //start scan the board
-      /*  File fileBoard = new File("dictionaryFile/small_board.txt");
-        try {
-            Scanner scan_Board = new Scanner(fileBoard);
-            String d = scan_Board.nextLine();
-            int dimension = Integer.parseInt(d);
-            String value;
-            String[] single_Value;
-            scan_Board.useDelimiter(" ");
-            Square[][] square = new Square[dimension][dimension];
-                for (int row = 0; row< dimension; row++) {
-                    for (int col = 0; col < dimension; col++) {
-                        //for(int t=0, t<2; t++){
-                            value = scan_Board.next();
-                            System.out.println(value);
-                        System.out.println("Value = "+ value + "; @ row = " + row + "; column = " + col );
-                        single_Value = value.split("");
-                        //System.out.println("single_Value[0] = " + single_Value[0]);
-                        //System.out.println("single_Value[1] = " + single_Value[1]);
-                        //System.out.println("----------------------------------------------------");
-                        square[row][col] = new Square(0, 0, false, null);
-                        if (single_Value[0].equals(".") && single_Value[1].equals(".")) {
-                            square[row][col].setWordMultiplier(0);
-                            square[row][col].setLetterMultiplier(0);
-                        } else if (single_Value[0].equals(".")) {
-                            square[row][col].setWordMultiplier(0);
-                            square[row][col].setLetterMultiplier(Integer.parseInt(single_Value[1]));
-                        } else if (single_Value[1].equals(".")) {
-                            square[row][col].setLetterMultiplier(0);
-                            square[row][col].setWordMultiplier(Integer.parseInt(single_Value[0]));
-                        }
-                        //System.out.println("square["+row+"]["+col+"] = "+square[row][col]);
-                        //System.out.print(square[row][col] + " ");
-                        scan_Boardscan_Board.nextLine();
-                    }
+
+            ///////////////////////////////////////////////////////////////////////////////////
+
+            //computer take up to 7 from scramble bag
+            Map<Integer, Tile> computerPlayerMap = new HashMap<>();
+            for (int i = 0; i < 7; i++) {
+                computerPlayer.take1(computerPlayerMap, listOfTiles);
+            }
+            System.out.println("computerplayer: " + computerPlayerMap.entrySet());
+            System.out.println("----------------------------------------");
+            //System.out.println("after both player take, Now bag left: "+ listOfTiles.entrySet());
+
+            //////////////////////////////////////////////////////////////////////////////////////////////
+            //player take up to 7 scrambles from scramble bag
+            Map<Integer, Tile> humanPlayerMap = new HashMap<>();
+            for (int i = 0; i < 7; i++) {
+                humanPlayer.take1(humanPlayerMap, listOfTiles);
+            }
+            System.out.println("humanPlayer: " + humanPlayerMap.entrySet());
+
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////
+            //human player add the tile to board
+            System.out.println("Please choose the key for the tile you want to add to the board: ");
+            Scanner scan_tile_key = new Scanner(System.in);
+            int tile_inputKey = scan_tile_key.nextInt();
+            System.out.println("You choose : " + humanPlayerMap.get(tile_inputKey));
+            scan_tile_key.close();
+
+
+            System.out.println(" please choose the row you want to put: ");
+            Scanner scan_row = new Scanner(System.in);
+            int row_input = scan_row.nextInt();
+            System.out.println("scan_row = " + row_input);
+            scan_row.close();
+
+            System.out.println(" please choose the column you want to put: ");
+            Scanner scan_column = new Scanner(System.in);
+            int column_input = scan_column.nextInt();
+            System.out.println("scan_column = " + column_input);
+            scan_column.close();
+
+            //add tile into the board
+            square[row_input][column_input].setTile(humanPlayerMap.get(tile_inputKey));
+            for (int i = 0; i < dimension; i++) {
+                for (int j = 0; j < dimension; j++) {
+                    System.out.print(square[i][j] + " ");
                 }
-                scan_Board.close();
+                System.out.println();
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }*/
-
-        ///////////////////////////////////////////////////////////////////////////////////
-        /*
-        //computer take up to 7 from scramble bag
-        Map<Integer, Tile> computerPlayerMap = new HashMap<>();
-        for(int i=0; i<7; i++){
-            computerPlayer.take1(computerPlayerMap, listOfTiles);
         }
-        System.out.println("computerplayer: "+ computerPlayerMap.entrySet());
-        System.out.println("----------------------------------------");
-        //System.out.println("after both player take, Now bag left: "+ listOfTiles.entrySet());
-
-        //////////////////////////////////////////////////////////////////////////////////////////////
-        //player take up to 7 scrambles from scramble bag
-        Map<Integer, Tile> humanPlayerMap = new HashMap<>();
-        for(int i=0; i<7; i++){
-            humanPlayer.take1(humanPlayerMap, listOfTiles);
-        }
-        System.out.println("humanPlayer: "+ humanPlayerMap.entrySet());
-
-
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////////
-        //human player add the tile to board
-        System.out.println("Please choose the key for the tile you want to add to the board: ");
-        Scanner scan_tile_key = new Scanner(System.in);
-        int tile_inputKey = scan_tile_key.nextInt();
-        System.out.println("You choose : "+humanPlayerMap.get(tile_inputKey));
-        scan_tile_key.close();
-
-
-        System.out.println(" please choose the row you want to put: ");
-        Scanner scan_row = new Scanner(System.in);
-        int row_input =  scan_row.nextInt();
-        System.out.println("scan_row = " + row_input);
-        scan_row.close();
-
-        System.out.println(" please choose the column you want to put: ");
-        Scanner scan_column = new Scanner(System.in);
-        int column_input = scan_column.nextInt();
-        System.out.println("scan_column = " + column_input);
-        scan_column.close();
-
-        //add tile into the board
-        square[row_input][column_input].setTile(humanPlayerMap.get(tile_inputKey));
-        for(int i= 0; i< dimension; i++){
-            for(int j=0; j< dimension; j++){
-                System.out.print(square[i][j] +" ");
-            }
-            System.out.println();
-        }
-
-
-    }
-    */
-
     }
 }
 
